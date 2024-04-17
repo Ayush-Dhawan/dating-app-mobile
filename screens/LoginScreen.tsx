bf1018489d4666b932b55d46f66f16ef71a37171
Ayush-Dhawan <ayushdhawan102004@gmail.com>
2024-04-17 21:13:29 +0530

    ui of login screen

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,10 +1,18 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
+import { View, Text, ImageBackground, StyleSheet, TouchableOpacity } from 'react-native'
+import React, { useLayoutEffect } from 'react'
 import { useAuthContext } from '../contexts/AuthContext'
 import { client } from '../utils/kindeConfig';
 import { storeData } from '../services/async-storage';
+import { useNavigation } from '@react-navigation/native';
 
 export default function LoginScreen() {
+
+const navigation = useNavigation();
+  useLayoutEffect(()=>{
+    navigation.setOptions({
+      headerShown : false
+    })
+  }, [])
 
   const {setLoggedIn} = useAuthContext();
 
@@ -20,9 +28,31 @@ export default function LoginScreen() {
     }
   };
   return (
-    <View>
-      <Text>LoginScreen</Text>
-      <Button title='Sign in with google' onPress={handleSignIn}  />
+    <View style={styles.container}>
+      <ImageBackground
+      style={{height: '100%'}}
+      source={{uri: "https://tinder.com/static/tinder.png"}}
+      >
+        <TouchableOpacity style={styles.button} onPress={handleSignIn}>
+          <Text style={{textAlign: 'center', fontWeight: '600'}}>Sign in & get swiping</Text>
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    width: '100%'
+  },
+  button : {
+    position: 'absolute',
+    bottom: 142,
+    right: '20%',
+    backgroundColor: 'white',
+    padding: 15,
+    width: '60%',
+    borderRadius: 99
+  }
+})
